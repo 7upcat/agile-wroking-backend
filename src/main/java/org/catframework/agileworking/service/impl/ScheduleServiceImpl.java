0370ceda36d3739f48fc6f5febf56650f297c954
zhengzheming <7upcat@gmail.com>
2017-08-10 22:23:16 +0800

    优化增加了排序，按会议开始日期排序

--- src/main/java/org/catframework/agileworking/service/impl/ScheduleServiceImpl.java
+++ src/main/java/org/catframework/agileworking/service/impl/ScheduleServiceImpl.java
@@ -3,9 +3,8 @@ package org.catframework.agileworking.service.impl;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.catframework.agileworking.domain.MeetingRoom;
 import org.catframework.agileworking.domain.MeetingRoomRepository;
 import org.catframework.agileworking.domain.Schedule;
@@ -17,8 +16,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleServiceImpl implements ScheduleService {
 
-	private static final Log logger = LogFactory.getLog(ScheduleServiceImpl.class);
-	
 	@Autowired
 	private ScheduleRepository scheduleRepository;
 
@@ -26,7 +23,7 @@ public class ScheduleServiceImpl implements ScheduleService {
 	private MeetingRoomRepository meetingRoomRepository;
 
 	@Override
-	public List<Schedule> find(Long meetingRoomId, Date date,Date to) {
+	public List<Schedule> find(Long meetingRoomId, Date date, Date to) {
 		MeetingRoom meetingRoom = meetingRoomRepository.findOne(meetingRoomId);
 		List<Schedule> schedules = scheduleRepository.findByMeetingRoomAndDate(meetingRoom, date);
 		List<Schedule> weeklySchedules = scheduleRepository.findByRepeatMode(Schedule.REPEAT_MODE_WEEKLY);
@@ -39,8 +36,6 @@ public class ScheduleServiceImpl implements ScheduleService {
 					c1.setTime(s1.getDate());
 					Calendar c2 = Calendar.getInstance();
 					c2.setTime(date);
-					logger.info(c1.getTime());
-					logger.info(c2.getTime());
 					if (c1.get(Calendar.DAY_OF_WEEK) == c2.get(Calendar.DAY_OF_WEEK)) {
 						s1.setDate(date);
 						schedules.add(s1);
@@ -48,7 +43,10 @@ public class ScheduleServiceImpl implements ScheduleService {
 				}
 			}
 		});
-		return schedules;
+		// 按照开始时间进行排序
+		return schedules.stream().sorted((s1, s2) -> {
+			return s1.getStartTime().compareTo(s2.getStartTime());
+		}).collect(Collectors.toList());
 	}
 
 	public void setScheduleRepository(ScheduleRepository scheduleRepository) {
